f060de6f466317629d681611a3f3be44fb687f21
mortenaho <mortenaho@outlook.com>
2020-01-17 16:06:16 +0330

    upload

---
+++ app/src/main/java/com/mortenaho/androidmvpexample/presenter/MainActivityPresenter.java
@@ -0,0 +1,34 @@
+package com.mortenaho.androidmvpexample.presenter;
+
+import com.mortenaho.androidmvpexample.model.User;
+
+public class MainActivityPresenter {
+
+    private User user;
+    private View view;
+
+    public MainActivityPresenter(View view) {
+        this.user = new User();
+        this.view = view;
+    }
+
+    public void updateFullName(String fullName){
+        user.setFullName(fullName);
+        view.updateUserInfoTextView(user.toString());
+
+    }
+
+    public void updateEmail(String email){
+        user.setEmail(email);
+        view.updateUserInfoTextView(user.toString());
+
+    }
+
+    public interface View{
+
+        void updateUserInfoTextView(String info);
+        void showProgressBar();
+        void hideProgressBar();
+
+    }
+}
---
+++ app/src/main/java/com/mortenaho/androidmvpexample/view/MainActivity.java
@@ -0,0 +1,100 @@
+package com.mortenaho.androidmvpexample.view;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.mortenaho.androidmvpexample.R;
+import com.mortenaho.androidmvpexample.presenter.MainActivityPresenter;
+
+public class MainActivity extends AppCompatActivity implements MainActivityPresenter.View {
+
+    private MainActivityPresenter presenter;
+    private TextView myTextView;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        presenter = new MainActivityPresenter(this);
+
+        myTextView = findViewById(R.id.myTextView);
+        EditText userName = findViewById(R.id.username);
+        EditText email = findViewById(R.id.email);
+        initProgressBar();
+
+
+        userName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                presenter.updateFullName(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                hideProgressBar();
+            }
+        });
+
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                presenter.updateEmail(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                hideProgressBar();
+            }
+        });
+
+    }
+
+    private void initProgressBar() {
+        progressBar = new ProgressBar(this, null, android.R.attr.progressBarStyleSmall);
+        progressBar.setIndeterminate(true);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(Resources.getSystem().getDisplayMetrics().widthPixels,
+                250);
+        params.addRule(RelativeLayout.CENTER_IN_PARENT);
+        this.addContentView(progressBar, params);
+        showProgressBar();
+    }
+
+    @Override
+    public void updateUserInfoTextView(String info) {
+        myTextView.setText(info);
+    }
+
+    @Override
+    public void showProgressBar() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgressBar() {
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+}
